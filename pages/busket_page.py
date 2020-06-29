6e9cee81197c23174944e814cf2a89dbd4eaba52
Azzy <8tyr@inbox.ru>
2020-06-29 18:28:57 +0300

    added tests for basket page

---
+++ pages/busket_page.py
@@ -0,0 +1,45 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.common.exceptions import NoAlertPresentException
+import math
+
+class ProductPage(BasePage):
+    
+    def add_product_to_basket(self):
+        basket_button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        basket_button.click()
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12*math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accep-()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+    
+    def right_product_price(self):
+        price_product = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT)
+        price_product_in_basket = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT_IN_BASKET)
+        assert price_product.text==price_product_in_basket.text, "product prices don't match"
+
+    def right_product_name(self):
+        name_product = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT)
+        name_product_in_basket = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT_IN_BASKET)
+        assert name_product.text==name_product_in_basket.text, "product names don't match"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+        "Success message is presented, but should not be"
+
+    def should_be_disappeared_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+        "Success message is presented, but should not be"
+
+
+    
