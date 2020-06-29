9e82abf42486836aee19b4ca679ad508f2ebefbd
Azzy <8tyr@inbox.ru>
2020-06-29 21:38:47 +0300

    added markers for login tests

--- pages/locators.py
+++ pages/locators.py
@@ -18,3 +18,8 @@ class ProductPageLocators ():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, 'div.basket-mini a')
+
+class BasketPageLocators():
+    EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner p")
+    PRODUCTS_IN_BASKET = (By.CSS_SELECTOR, ".basket_summary")
